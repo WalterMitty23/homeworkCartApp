984042ba3286215629244a591255251b554c1a02
WalterMitty23 <23alexold23@gmail.com>
2024-02-19 07:15:08 -0500

    Just started

---
+++ src/main/java/com/homework/springCartApp/SpringCartAppApplication.java
@@ -0,0 +1,13 @@
+package com.homework.springCartApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCartAppApplication.class, args);
+	}
+
+}
